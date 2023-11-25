bbe2e55ebd9d748a0ae781ef835d1f06c1b78c7f
Rustam180292 <141083956+Rustam180292@users.noreply.github.com>
2023-11-26 00:38:15 +0500

    dd
    dd

--- js/main.js
+++ js/main.js
@@ -1,12 +1,2 @@
-const minVal = document.querySelector(".min-val");
-const maxVal = document.querySelector(".max-val");
-const priceInputMin = document.querySelector(".min-input");
-const priceInputMax = document.querySelector(".max-input");
-const minTooltip = document.querySelector(".min-tooltip");
-const maxTooltip = document.querySelector(".max-tooltip");
-const minGap = 0;
-const range = document.querySelector(".slider-track");
-const sliderMinValue = parseInt(minVal.min);
-const sliderMaxValue = parseInt(maxVal.max);
 
 
