76db8bca2438c8b143cc9735acc98409b7fcd405
Rustam180292 <141083956+Rustam180292@users.noreply.github.com>
2023-11-27 10:31:56 +0500

    swiper
    almost done

--- js/main.js
+++ js/main.js
@@ -38,5 +38,28 @@ rangeInput.forEach(input =>{
 });
 
 
-
+// SWIPER //
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    
+    loop: true,
+  
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+    slidesPerView: 3
+  });
+  
 
